55f008fa67d8b06a6e36f95dbd6d1bb1a18d30db
Sober <59960384+Soberek@users.noreply.github.com>
2025-04-21 13:35:54 +0000

    Apr 21, 2025, 3:35 PM

--- backend/src/routes/schoolRouter.ts
+++ backend/src/routes/schoolRouter.ts
@@ -5,3 +5,6 @@ const schoolRouter = Router();
 const schoolController = new SchoolController();
 
 schoolRouter.get("/", schoolController.getAllSchools);
+schoolRouter.post("/", schoolController.createSchool);
+// TODO schoolRouter.delete("/:id", schoolController.deleteSchool);
+// TODO schoolRouter.put("/:id", schoolController.updateSchool);
