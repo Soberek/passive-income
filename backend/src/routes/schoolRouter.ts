452d6c1124f5f475f30ba1926c2cd6acd4a3c81b
Sober <59960384+Soberek@users.noreply.github.com>
2025-04-21 13:58:30 +0000

    Apr 21, 2025, 3:58 PM

--- backend/src/routes/schoolRouter.ts
+++ backend/src/routes/schoolRouter.ts
@@ -4,9 +4,13 @@ import SchoolController from "../controllers/schoolController";
 const schoolRouter = Router();
 const schoolController = new SchoolController();
 
-schoolRouter.get("/school", schoolController.getAllSchools);
-schoolRouter.post("/school", schoolController.createSchool);
+// TODO schoolRouter.get("/:id", schoolController.getSchoolById);
 // TODO schoolRouter.delete("/:id", schoolController.deleteSchool);
 // TODO schoolRouter.put("/:id", schoolController.updateSchool);
+
+// Route: /api/school - GET - Get all schools
+schoolRouter.get("/school", schoolController.getAllSchools);
+// Route: /api/school - POST - Create a new school
+schoolRouter.post("/school", schoolController.createSchool);
 
 export default schoolRouter;
