362d9c60f79743173024b8444b991816a576b922
Sober <59960384+Soberek@users.noreply.github.com>
2025-04-27 10:13:28 +0000

    Apr 27, 2025, 12:13 PM

--- passive-income-frontend/src/App.tsx
+++ passive-income-frontend/src/App.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [data, setData] = useState<any>(null);
 
   const URL = "http://127.0.0.1:3000";
