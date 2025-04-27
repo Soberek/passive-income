b15da1d8dc8a07925c3fdea5b55a96ad73d421c1
Sober <59960384+Soberek@users.noreply.github.com>
2025-04-27 12:43:52 +0000

    Apr 27, 2025, 2:43 PM

--- passive-income-frontend/src/schools/SchoolList.tsx
+++ passive-income-frontend/src/schools/SchoolList.tsx
@@ -4,11 +4,37 @@ export const SchoolList = ({ schools }: { schools: SchoolInstitution[] }) => {
   return (
     <div>
       <h1>School List</h1>
-      <ul>
-        {schools.map((school) => (
-          <li key={school.id.toString()}>{school.name}</li>
-        ))}
-      </ul>
+
+      <table style={{ width: "100%", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Director</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>Postal Code</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Website</th>
+            <th>Municipality</th>
+          </tr>
+        </thead>
+        <tbody>
+          {schools.map((school) => (
+            <tr key={school.id.toString()}>
+              <td>{school.name}</td>
+              <td>{school.director}</td>
+              <td>{school.address}</td>
+              <td>{school.city}</td>
+              <td>{school.postalCode}</td>
+              <td>{school.phone}</td>
+              <td>{school.email}</td>
+              <td>{school.website}</td>
+              <td>{school.municipality}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
