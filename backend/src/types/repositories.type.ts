781c39c06d2d2c02d9db90aca026fca2f85823f3
Sober <59960384+Soberek@users.noreply.github.com>
2025-05-16 15:58:43 +0200

    Update add method in RepositoryI interface to allow for BigInt and null return types

--- backend/src/types/repositories.type.ts
+++ backend/src/types/repositories.type.ts
@@ -1,7 +1,7 @@
 export interface RepositoryI<T> {
   getAll: () => T[];
   getById: (id: number) => T | null;
-  add: (entity: Partial<T>) => number;
+  add: (entity: Partial<T>) => number | BigInt | null;
   delete: (id: number) => boolean;
   update: (id: number, entity: Partial<T>) => boolean;
 }
