2c119e5f50958d64805c215f4bf2b020b5316ddb
Sober <59960384+Soberek@users.noreply.github.com>
2025-05-17 19:38:52 +0200

    Refactor ContactService to implement ServiceI interface, streamline method names, and enhance validation logic

--- backend/src/services/contact.service.ts
+++ backend/src/services/contact.service.ts
@@ -1,69 +1,42 @@
 import { Contact } from "../../../shared/types";
 import { ContactModel } from "../models/contact.model";
 import { ContactRepository } from "../repositories/contact.repository";
+import { ServiceI } from "../types/index.type";
 
-// Model is a class that represents the structure of a contact. Used to create a new contact and validate the data.
-// It implements the Contact interface, which defines the properties of a contact.
-
-// Repository is a class that handles the database operations related to contacts.
-// It uses the sqliteDbService to interact with the SQLite database.
-
-// Service is a class that provides higher-level operations related to contacts.
-// It uses the ContactRepository to perform CRUD operations on contacts.
-// It can also include business logic related to contacts like validation, formatting, etc. So it use the ContactModel class to create a new contact and validate the data and repository to perform CRUD operations on contacts.
-
-// Controller is a class that handles the HTTP requests related to contacts.
-// It uses the ContactService to perform operations on contacts.
-// It can also include request validation, response formatting, etc. Joi
-
-export interface ContactServiceI {
-  getAllContacts: () => Contact[];
-  addNewContact: (entity: Omit<Contact, "contactId">) => number | BigInt | null;
-  getContactById: (id: number) => Contact | null;
-  updateContact: (id: number, entity: Partial<Contact>) => boolean;
-  deleteContact: (id: number) => boolean;
-}
-export class ContactService implements ContactServiceI {
+export class ContactService implements ServiceI<Contact, "contactId", number | BigInt> {
   constructor(private contactRepository: ContactRepository) {
     this.contactRepository = contactRepository;
   }
 
-  public getAllContacts = (): Contact[] => {
+  public getAll = (): Contact[] => {
     return this.contactRepository.getAll();
   };
 
-  public addNewContact = (entity: Omit<Contact, "contactId">) => {
-    const contact = new ContactModel(entity.firstName, entity.lastName, entity.email || "", entity.phone || "");
-    const validationErrors = contact.validate();
+  public add = (entity: Omit<Contact, "contactId">) => {
+    const validationErrors = ContactModel.validate(entity);
 
     if (validationErrors.length) {
       throw new Error("Invalid data: " + validationErrors.join(", "));
     }
 
-    return this.contactRepository.add(contact);
+    return this.contactRepository.add(entity);
   };
 
-  public getContactById = (id: number): Contact | null => {
+  public getById = (id: number | BigInt): Contact | null => {
     return this.contactRepository.getById(id);
   };
 
-  public updateContact = (id: number, entity: Partial<Contact>) => {
-    const contact = new ContactModel(
-      entity.firstName || "",
-      entity.lastName || "",
-      entity.email || "",
-      entity.phone || ""
-    );
-    const validationErrors = contact.validate();
+  public update = (id: number | BigInt, entity: Partial<Contact>) => {
+    const validationErrors = ContactModel.validate(entity);
 
     if (validationErrors.length) {
       throw new Error("Invalid data: " + validationErrors.join(", "));
     }
 
-    return this.contactRepository.update(id, contact);
+    return this.contactRepository.update(id, entity);
   };
 
-  public deleteContact = (id: number) => {
+  public delete = (id: number | BigInt) => {
     return this.contactRepository.delete(id);
   };
 
