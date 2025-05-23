dffd4a423c9d136f350dc8700ce33db620a23528
Sober <59960384+Soberek@users.noreply.github.com>
2025-05-23 19:19:46 +0200

    ✨ Add MenuAppBar and NavDrawer components for improved navigation

--- passive-income-frontend/src/App.tsx
+++ passive-income-frontend/src/App.tsx
@@ -1,44 +1,11 @@
 import { Outlet } from "react-router";
-import { NavLink } from "react-router";
 
-const routes = [
-  {
-    name: "Szkoły",
-    path: "schools",
-  },
-  {
-    name: "Kontakty",
-    path: "contacts",
-  },
-  {
-    name: "Izrz offline",
-    path: "izrz",
-  },
-];
+import MenuAppBar from "./components/AppBar";
+
 function App() {
   return (
     <>
-      <nav style={{ display: "flex", gap: "10px", justifyContent: "center", marginBottom: "20px" }}>
-        <ul style={{ listStyleType: "none", padding: 0, margin: 0, display: "flex", gap: "10px" }}>
-          {routes.map((route) => (
-            <li key={route.path}>
-              <NavLink
-                to={route.path}
-                style={({ isActive }) => ({
-                  padding: "10px",
-                  textAlign: "center",
-                  fontWeight: "bold",
-                  fontSize: "1.5rem",
-                  textDecoration: "none",
-                  color: isActive ? "blue" : "black",
-                })}
-              >
-                {route.name}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <MenuAppBar />
       <Outlet />
     </>
   );
---
+++ passive-income-frontend/src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { NavLink } from "react-router";
+
+const routes = [
+  {
+    icon: "🏫",
+    name: "Dodaj szkołę",
+    path: "schools",
+  },
+  {
+    icon: "📞",
+    name: "Dodaj kontakt",
+    path: "contacts",
+  },
+  {
+    icon: "📊",
+    name: "Wygeneruj izrz offline",
+    path: "izrz",
+  },
+];
+
+export default function NavDrawer({
+  isOpen,
+  toggleDrawer,
+}: {
+  isOpen: boolean;
+  toggleDrawer: (newOpen: boolean) => () => void;
+}) {
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {routes.map((route) => (
+          <ListItem key={route.path} disablePadding>
+            <ListItemButton component={NavLink} to={route.path}>
+              <ListItemIcon>{route.icon}</ListItemIcon>
+              <ListItemText primary={route.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Box>
+  );
+
+  return (
+    <div style={{ display: "flex", marginBottom: "20px", marginTop: "20px" }}>
+      {/* <Button onClick={toggleDrawer(true)} variant="contained">
+        Otwórz menu
+      </Button> */}
+      <Drawer open={isOpen} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
