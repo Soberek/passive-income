e95c76eab83ad2e1868c8394a07e142fa88ae2fe
Sober <59960384+Soberek@users.noreply.github.com>
2025-05-23 17:51:13 +0200

    ✨ Define Material, SchoolYear, and Task interfaces in types

--- shared/types/index.d.ts
+++ shared/types/index.d.ts
@@ -44,3 +44,29 @@ export interface ActionType {
   actionTypeId: number | BigInt;
   name: string;
 }
+
+export interface Material {
+  materialId: number | BigInt;
+  name: string;
+  type: "ulotka" | "plakat" | "broszura" | "zakładka" | "inne";
+  description: string;
+}
+
+export interface SchoolYear {
+  schoolYearId: number | BigInt;
+  year: "2024/2025" | "2025/2026" | "2026/2027" | "2027/2028";
+}
+
+export interface Task {
+  taskId: number | BigInt;
+  referenceNumber: string;
+  taskNumber?: string; // unique task number like "1/2025"
+  institutionId: number | BigInt;
+  programId: number | BigInt;
+  actionTypeId: number | BigInt;
+  description?: string;
+  date: Date; // YYYY-MM-DD
+  actionsCount: number;
+  audienceCount: number;
+  mediaPlatformId?: number | BigInt; // will be used if the task is a media publication
+}
