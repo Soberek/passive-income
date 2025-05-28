0a3853111b57cc82199ba7d9b27cc0e7de5876d8
Sober <59960384+Soberek@users.noreply.github.com>
2025-05-28 19:13:13 +0200

    Refactor useFetch hook to manage loading state and errors with useState and useEffect

--- passive-income-frontend/src/hooks/useFetch.ts
+++ passive-income-frontend/src/hooks/useFetch.ts
@@ -1,13 +1,30 @@
-export const useFetch = <T>(url: string, options?: RequestInit): Promise<T> => {
-  return new Promise((resolve, reject) => {
-    fetch(url, options)
-      .then((response) => {
+import { useEffect, useState } from "react";
+
+export const useFetch = <T>(url: string, options?: RequestInit) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-        return response.json();
-      })
-      .then((data: T) => resolve(data))
-      .catch((error) => reject(error));
-  });
+        const result: T = await response.json();
+
+        console.log("Fetched data:", result);
+        setData(result);
+      } catch (error) {
+        setError(error instanceof Error ? error : new Error("Unknown error"));
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url, options]);
+
+  return { data, error, loading };
 };
