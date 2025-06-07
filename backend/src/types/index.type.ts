b47716b10c65e0336c0d692b2c1f53393e5be8f1
Sober <59960384+Soberek@users.noreply.github.com>
2025-06-07 11:28:01 +0200

    Refactor repository interfaces to implement interface segregation for improved clarity and maintainability

--- backend/src/types/index.type.ts
+++ backend/src/types/index.type.ts
@@ -6,6 +6,26 @@ export interface RepositoryI<T, idKey extends keyof T, idType = number> {
   update: (id: idType, entity: Partial<T>) => boolean;
 }
 
+export interface CreatableIRepositoryI<T, idKey extends keyof T, idType = number> {
+  add: (entity: Omit<T, idKey>) => idType | null;
+}
+export interface ReadableRepositoryI<T, idKey extends keyof T, idType = number> {
+  getAll: () => T[];
+  getById: (id: idType) => T | null;
+}
+
+export interface DeletableRepositoryI<T, idKey extends keyof T, idType = number> {
+  delete: (id: idType) => boolean;
+}
+
+export interface UpdatableRepositoryI<T, idKey extends keyof T, idType = number> {
+  update: (id: idType, entity: Partial<T>) => boolean;
+}
+
+export interface BulkInsertableRepositoryI<T, idKey extends keyof T, idType = number> {
+  bulkInsert: (entities: Omit<T, idKey>[]) => boolean;
+}
+
 // bad
 export interface ServiceI<T, idKey extends keyof T, idType = number> {
   getAll: () => T[];
