8c44171016281d5da71a7aba70fbd516a5aad208
Sober <59960384+Soberek@users.noreply.github.com>
2025-06-07 11:27:19 +0200

    Refactor SqliteDbService to improve class structure and maintainability

--- backend/src/services/sqlite_db.service.ts
+++ backend/src/services/sqlite_db.service.ts
@@ -1,93 +1,38 @@
-import betterSqlite3 from "better-sqlite3";
+import SqliteDatabase from "better-sqlite3";
 
 import path from "path";
 
-// how to get current path
 const DB_PATH = path.resolve(__dirname, "../../sqliteDb.db");
-const DB_PATH_TEST = path.resolve(__dirname, "../../sqliteDbTest.db");
 
-// import { v4 as uuidv4 } from "uuid";
+import type { DatabaseI } from "../types/database.type";
 
-class sqliteDbService {
-  private db: betterSqlite3.Database;
-  private static instance: sqliteDbService | null = null;
+// sqlite DbService is a singleton class that provides a connection to a SQLite database using better-sqlite3.
+// It implements the DatabaseServiceI interface, which defines methods for interacting with the database.
 
-  // The constructor takes an object with a dbPath property, which is the path to the SQLite database file.
-  // constructor is a special method for creating and initializing an object created with a class.
-  private constructor(params: { dbPath: string; test?: boolean }) {
-    this.db = betterSqlite3(params.dbPath, {
-      verbose: console.log, // Enable verbose logging for debugging
-    });
+export class SqliteDbService implements DatabaseI<SqliteDatabase.Database> {
+  private static instance: SqliteDbService | null = null;
+  private db: SqliteDatabase.Database;
+
+  private constructor(dbPath: string) {
+    this.db = new SqliteDatabase(dbPath);
   }
 
-  // zwraca instancję sqliteDbService
-  // singleton pattern zeby nie tworzyc nowej instancji za kazdym razem
-  // bede przekazywal do kazdego service czy kontrolera
-  public static getInstance = (test: boolean = false): sqliteDbService => {
-    const dbPath = test ? DB_PATH_TEST : DB_PATH;
-
-    // Always create a new instance for tests
-    if (test) {
-      console.log("Creating new instance for test");
-      return new sqliteDbService({ dbPath });
+  public static getInstance(): SqliteDbService {
+    if (!this.instance) {
+      this.instance = new SqliteDbService(DB_PATH);
     }
+    return this.instance;
+  }
 
-    // Use the singleton instance for production
-    if (!sqliteDbService.instance) {
-      console.log("Creating new instance");
-      sqliteDbService.instance = new sqliteDbService({ dbPath });
-      return sqliteDbService.instance;
-    }
-
-    return sqliteDbService.instance;
-  };
-
-  public static resetInstance = (): void => {
-    sqliteDbService.instance = null; // Clear the singleton instance
-  };
-
-  prepare = (sql: string) => {
-    try {
-      const stmt = this.db.prepare(sql);
-      return stmt;
-    } catch (error) {
-      throw new Error(`Error preparing SQL statement: ${error instanceof Error ? error.message : String(error)}`);
-    }
-  };
-
-  // zwraca wszystkie rekordy z tabeli o podanej nazwie
-  // getTable
-  getTable = <T>(tableName: string): T[] => {
-    try {
-      const stmt = this.db.prepare(`SELECT * FROM ${tableName}`);
-      return stmt.all() as T[];
-    } catch (error) {
-      throw new Error(
-        `Error fetching records from ${tableName}: ${error instanceof Error ? error.message : String(error)}`
-      );
-    }
-  };
-
-  close = () => {
-    try {
-      this.db.close();
-    } catch (error) {
-      throw new Error(`Error closing database connection: ${error instanceof Error ? error.message : String(error)}`);
-    }
-  };
-
-  getDb = () => {
+  getDb() {
     return this.db;
-  };
-
-  transaction = <T>(callback: () => T) => {
-    try {
-      const transaction = this.db.transaction(callback);
-      return transaction;
-    } catch (error) {
-      throw new Error(`Error executing transaction: ${error instanceof Error ? error.message : String(error)}`);
-    }
-  };
+  }
+  close() {
+    this.db.close();
+  }
+  transaction<T>(callback: () => T): T {
+    return this.db.transaction(callback)();
+  }
 }
 
-export default sqliteDbService;
+export default SqliteDbService;
