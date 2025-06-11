bf5727c94f0d636b91c40c3ad37dc31a0316a6f5
Sober <59960384+Soberek@users.noreply.github.com>
2025-06-11 18:23:19 +0200

    Refactor SchoolProgramParticipationController to implement centralized error handling and standardize JSON responses with 'data' wrapper

--- backend/src/modules/school-program-participation/school_program_participation.controller.ts
+++ backend/src/modules/school-program-participation/school_program_participation.controller.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import SchoolProgramParticipationService from "./school_program_participation.service";
-
+import { AppError } from "../../handlers/error.handler";
 class SchoolProgramParticipationController {
   constructor(private schoolProgramParticipationService: SchoolProgramParticipationService) {}
 
-  addSchoolProgramParticipation = (req: Request, res: Response): void => {
+  addSchoolProgramParticipation = (req: Request, res: Response, next: NextFunction): void => {
     const { schoolId, programId, schoolYearId } = req.body;
     console.log(req.body);
     console.log(schoolId, programId, schoolYearId);
@@ -12,9 +12,11 @@ class SchoolProgramParticipationController {
 
     console.log("result: ", result);
     if (result) {
-      res.status(201).send("School program participation added successfully");
+      res.status(201).send({ message: "School program participation added successfully", data: result });
+      return;
     } else {
-      res.status(400).send("Error adding school program participation");
+      next(new AppError("Error adding school program participation", 400));
+      return;
     }
   };
 }
