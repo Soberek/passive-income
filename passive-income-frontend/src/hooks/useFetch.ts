502d3883a54c53958987b3e081654bd573d5ff25
Sober <59960384+Soberek@users.noreply.github.com>
2025-06-13 12:05:59 +0200

    Refactor useFetch hook to encapsulate fetch logic and improve data handling with refetch capability

--- passive-income-frontend/src/hooks/useFetch.ts
+++ passive-income-frontend/src/hooks/useFetch.ts
@@ -4,19 +4,26 @@ export const useFetch = <T>(url: string, options?: RequestInit) => {
   const [data, setData] = useState<T | null>(null);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+
+  async function fetchData() {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const result = await response.json();
+
+    console.log("Fetched data:", result);
+    return result.data;
+  }
+
   useEffect(() => {
     setLoading(true);
     setError(null);
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const result = await response.json();
 
-        console.log("Fetched data:", result);
-        setData(result.data);
+    const fetchDataEffect = async () => {
+      try {
+        const data = await fetchData();
+        setData(data);
       } catch (error) {
         console.log("Fetch error:", error);
         setError(error instanceof Error ? error : new Error("Unknown error"));
@@ -24,8 +31,24 @@ export const useFetch = <T>(url: string, options?: RequestInit) => {
         setLoading(false);
       }
     };
-    fetchData();
+
+    fetchDataEffect();
   }, [url]);
 
-  return { data, error, loading };
+  const refetch = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const newData = await fetchData();
+      setData(newData);
+    } catch (error) {
+      console.log("Refetch error:", error);
+      setError(error instanceof Error ? error : new Error("Unknown error"));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, error, loading, refetch };
 };
