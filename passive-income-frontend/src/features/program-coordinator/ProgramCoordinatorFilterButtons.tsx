a5be463f54e1ba339444e9a8db563ac5024f80ad
Sober <59960384+Soberek@users.noreply.github.com>
2025-06-14 15:32:25 +0200

    Refactor ProgramCoordinatorFilters component to improve layout and enhance user experience with better structure and loading messages

--- passive-income-frontend/src/features/program-coordinator/ProgramCoordinatorFilterButtons.tsx
+++ passive-income-frontend/src/features/program-coordinator/ProgramCoordinatorFilterButtons.tsx
@@ -1,4 +1,4 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
 
 import { Contact, Institution, Program, SchoolYear } from "../../../../shared/types";
 import { FormValues } from "./ProgramCoordinator";
@@ -48,75 +48,80 @@ export const ProgramCoordinatorFilters: React.FC<ProgramCoordinatorFiltersProps>
   }
 
   return (
-    <div style={{ display: "flex", gap: "16px", marginBottom: "16px" }}>
-      <FormControl fullWidth>
-        <InputLabel id="school-year-label">Rok szkolny</InputLabel>
-        <Select
-          labelId="school-year-label"
-          value={schoolYearIdParam ?? ""}
-          label="Rok szkolny"
-          onChange={(e) => handleParamsChange("schoolYearId", e.target.value)}
-        >
-          <MenuItem value="">All</MenuItem>
-          {schoolYears.map((schoolYear) => (
-            <MenuItem key={schoolYear.schoolYearId} value={schoolYear.schoolYearId}>
-              {schoolYear.year}
+    <>
+      <Typography padding={2} variant="h6" component="div" style={{ flexGrow: 1 }}>
+        Filtry
+      </Typography>
+      <div style={{ padding: "16px", display: "flex", gap: "16px", marginBottom: "16px" }}>
+        <FormControl fullWidth>
+          <InputLabel id="school-year-label">Rok szkolny</InputLabel>
+          <Select
+            labelId="school-year-label"
+            value={schoolYearIdParam ?? ""}
+            label="Rok szkolny"
+            onChange={(e) => handleParamsChange("schoolYearId", e.target.value)}
+          >
+            <MenuItem value="">All</MenuItem>
+            {schoolYears.map((schoolYear) => (
+              <MenuItem key={schoolYear.schoolYearId} value={schoolYear.schoolYearId}>
+                {schoolYear.year}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="program-label">Program</InputLabel>
+          <Select
+            labelId="program-label"
+            value={programIdParam || ""}
+            label="Program"
+            defaultValue={programIdParam || ""}
+            onChange={(e) => handleParamsChange("programId", e.target.value)}
+          >
+            <MenuItem value="" disabled defaultValue={programIdParam || ""}>
+              Wybierz program
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="program-label">Program</InputLabel>
-        <Select
-          labelId="program-label"
-          value={programIdParam || ""}
-          label="Program"
-          defaultValue={programIdParam || ""}
-          onChange={(e) => handleParamsChange("programId", e.target.value)}
-        >
-          <MenuItem value="" disabled defaultValue={programIdParam || ""}>
-            Wybierz program
-          </MenuItem>
-          <MenuItem value="">All</MenuItem>
-          {programs.map((program) => (
-            <MenuItem key={program.programId} value={program.programId}>
-              {program.name}
+            <MenuItem value="">All</MenuItem>
+            {programs.map((program) => (
+              <MenuItem key={program.programId} value={program.programId}>
+                {program.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="institution-label">Instytucja</InputLabel>
+          <Select
+            value={institutionIdParam || ""}
+            label="Instytucja"
+            onChange={(e) => handleParamsChange("institutionId", e.target.value)}
+          >
+            {institutions.map((institution) => (
+              <MenuItem key={institution.institutionId} value={institution.institutionId}>
+                {institution.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="contact-label">Kontakt</InputLabel>
+          <Select
+            value={contactIdParam || ""}
+            label="Kontakt"
+            onChange={(e) => handleParamsChange("contactId", e.target.value)}
+          >
+            <MenuItem value="" disabled>
+              Wybierz kontakt
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="institution-label">Instytucja</InputLabel>
-        <Select
-          value={institutionIdParam || ""}
-          label="Instytucja"
-          onChange={(e) => handleParamsChange("institutionId", e.target.value)}
-        >
-          {institutions.map((institution) => (
-            <MenuItem key={institution.institutionId} value={institution.institutionId}>
-              {institution.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="contact-label">Kontakt</InputLabel>
-        <Select
-          value={contactIdParam || ""}
-          label="Kontakt"
-          onChange={(e) => handleParamsChange("contactId", e.target.value)}
-        >
-          <MenuItem value="" disabled>
-            Wybierz kontakt
-          </MenuItem>
-          <MenuItem value="">All</MenuItem>
-          {contacts.map((contact: Contact) => (
-            <MenuItem key={contact.contactId} value={contact.contactId}>
-              {contact.firstName} {contact.lastName}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
+            <MenuItem value="">All</MenuItem>
+            {contacts.map((contact: Contact) => (
+              <MenuItem key={contact.contactId} value={contact.contactId}>
+                {contact.firstName} {contact.lastName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    </>
   );
 };
