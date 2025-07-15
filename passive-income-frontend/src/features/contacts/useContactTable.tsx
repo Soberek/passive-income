efe089191419822b448072cc0c61259e9ca483b7
Sober <59960384+Soberek@users.noreply.github.com>
2025-07-15 11:51:20 +0200

    simple rhf form

--- passive-income-frontend/src/features/contacts/useContactTable.tsx
+++ passive-income-frontend/src/features/contacts/useContactTable.tsx
@@ -1,8 +1,29 @@
 import { useFetch } from "../../hooks/useFetch";
 import { Contact } from "../../../../shared/types";
+import { useForm } from "react-hook-form";
 
 export const useContactTable = () => {
   const { data, error, loading } = useFetch<Contact[]>("http://localhost:3000/api/contacts");
 
   return { data, error, loading };
 };
+
+function temporaryForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Contact>();
+
+  const onSubmit = (data: Contact) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("firstName", { required: true })} />
+      {errors.firstName && <span>This field is required</span>}
+      <input type="submit" />
+    </form>
+  );
+}
